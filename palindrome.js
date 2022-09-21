5c77f1e11a2afaa62840aa7719de138d689e5d4a
abhishek1494k <abhi.jaiswal1494@gmail.com>
2022-09-21 07:38:00 +0530

    m2_code_fix_Jaiswal

--- palindrome.js
+++ palindrome.js
@@ -1,8 +1,12 @@
 function palindrome(string){
-    let sum="";
-    for(let i=string.length-1;i>=0;i--){
-        sum+=string[i];
-    }console.log(sum) 
 
-    }
-    palindrome("akash")
+   let sum="";
+
+     for(let i=string.length-1;i>=0;i--){
+        
+	sum+=string[i];
+
+    }     console.log(sum) 
+    };
+
+palindrome("akash chaurasia")
